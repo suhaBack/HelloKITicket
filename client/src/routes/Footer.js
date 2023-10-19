01d8809c176336df9dde946a63073dc1190f5a9b
YUJUN <iiww123@naver.com>
2023-10-19 17:33:18 +0900

    123

--- client/src/routes/Footer.js
+++ client/src/routes/Footer.js
@@ -3,8 +3,8 @@ import "../css/footer.css";
 
 const Footer = () => {
     return (
-            <div id ="footer">
-                <div id="footer-copy">
+            <div id="kitiket-down">
+                <div class="kitiket-content-wrapper"><div id="kitiket-footer-copy">
                     <ul>    
                         <li>홈</li> 
                         <li>헬로키티켓 안내</li>
@@ -12,16 +12,11 @@ const Footer = () => {
                         <li>개인정보처리방침</li>
                     </ul>
                 </div>
-                <div id="footer-address">
-                    <span>㈜헬로키티켓</span>
-                    <span>서울특별시 용산구 남산공원길 105</span>
-                    <span>사업자 등록번호 : 101-202-30303</span>
-                    <br />
-                    <span>통신판매업신고번호 : 2023-남산타워B-0003</span>
-                    <span>고객센터(평일/주말 09:00~18:00) : 1011-0220</span>
-                    <br />
+                <div id="kitiket-footer-address">
+                    <span>㈜헬로키티켓 | 서울특별시 용산구 남산공원길 105 | 사업자 등록번호 : 101-202-30303</span>
+                    <span>통신판매업신고번호 : 2023-남산타워B-0003 | 고객센터(평일/주말 09:00~18:00) : 1011-0220</span>
                     <span>© Kitiket Entertainment Corp.</span>
-                </div>
+                </div></div>
             </div>
     );
 };
